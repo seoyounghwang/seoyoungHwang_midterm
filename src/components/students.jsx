3806461e04cd5509132056ad27f313ddc027e47c
SeoyoungHwang <dev.seoyoung.hwang@gmail.com>
2022-06-27 18:09:43 -0600

    generates state and creates student child component

--- src/components/students.jsx
+++ src/components/students.jsx
@@ -1,10 +1,43 @@
 import React, { Component } from 'react';
+import Student from './student';
 
 class Students extends Component {
+
+    state = {
+        cardInfo : [
+            {
+              image:
+                "s1.png",
+              name: "Mike",
+              gpa: 3.8
+            },
+            {
+              image:
+                "s2.png",
+                name: "Manal",
+                gpa: 3.6
+            },
+            {
+              image:
+                "s3.png",
+                name: "Moe",
+                gpa: 3.2
+            },
+            {
+              image:
+                "s4.png",
+                name: "Mary",
+                gpa: 2.6
+            }
+          ],
+        
+    }
     render() {
         return (
             <div>
-                Students.jsx
+                {this.state.cardInfo.map(student=> (
+                    <Student key={student.image} student={student}/> 
+                ))}
             </div>
         );
     }
