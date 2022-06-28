8845861ca7409e7916a73112ff3af6ba8b2c1835
SeoyoungHwang <dev.seoyoung.hwang@gmail.com>
2022-06-27 18:11:16 -0600

    displays student component using props

--- src/components/student.jsx
+++ src/components/student.jsx
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 
 class Student extends Component {
     render() {
+        const {image, name, gpa} = this.props.student;
         return (
-            <div>
-                This is Student
-            </div>
+            <>
+            <img src={"./image/"+image} alt='profile'/>
+            <p className="student-name">{name}</p>
+            <p className="student-gpa">{gpa}</p>
+            </>
         );
     }
 }
