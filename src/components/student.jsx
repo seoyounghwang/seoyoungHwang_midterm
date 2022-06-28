008999902ba2c1f19794e55fe90e74d3794c1f10
SeoyoungHwang <dev.seoyoung.hwang@gmail.com>
2022-06-27 20:35:37 -0600

    install bootstrap

--- src/components/student.jsx
+++ src/components/student.jsx
@@ -4,11 +4,12 @@ class Student extends Component {
     render() {
         const {image, name, gpa} = this.props.student;
         return (
-            <>
+            <div>
             <img src={"./image/"+image} alt='profile'/>
             <p className="student-name">{name}</p>
             <p className="student-gpa">{gpa}</p>
-            </>
+            <button className="student-grade">Grade</button>
+            </div>
         );
     }
 }
