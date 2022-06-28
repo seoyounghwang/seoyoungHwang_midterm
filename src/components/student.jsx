20c29779f07682050bd13435f8e93b9ba9683630
SeoyoungHwang <dev.seoyoung.hwang@gmail.com>
2022-06-27 18:07:58 -0600

    creates student & students .jsx

---
+++ src/components/student.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Student extends Component {
+    render() {
+        return (
+            <div>
+                This is Student
+            </div>
+        );
+    }
+}
+
+export default Student;
---
+++ src/components/students.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Students extends Component {
+    render() {
+        return (
+            <div>
+                Students.jsx
+            </div>
+        );
+    }
+}
+
+export default Students;
